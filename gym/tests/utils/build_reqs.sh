be257466361602c7789e8b80b4096181a09659be
raphaelvrosa <raphaelvrosa@gmail.com>
2019-05-14 12:08:19 -0300

    Update build_reqs.sh
    Adds pcap files to be downloaded when building requirements for test cases 1, 2 and 3.

--- gym/tests/utils/build_reqs.sh
+++ gym/tests/utils/build_reqs.sh
@@ -1,4 +1,5 @@
 echo "This scripts clones and builds the images needed for the gym test case 1, 2 and 3"
+echo "And download pcap files needed for test case 3"
 
 git clone https://github.com/raphaelvrosa/gym-vnfs
 
@@ -7,3 +8,14 @@ cd gym-vnfs
 sudo chmod +x build.sh
 
 sudo ./build.sh
+
+echo "Downloading pcap files for test case 3"
+wget https://s3.amazonaws.com/tcpreplay-pcap-files/smallFlows.pcap 
+wget https://s3.amazonaws.com/tcpreplay-pcap-files/bigFlows.pcap
+
+sudo mkdir -p /mnt/pcaps
+sudo mv smallFlows.pcap /mnt/pcaps/
+sudo mv bigFlows.pcap /mnt/pcaps/
+
+echo "Added pcap files smallFlows.pcap and bigFlows.pcap to /mnt/pcaps/ folder"
+
